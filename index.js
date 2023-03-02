1002165f0ca5e8752fc98ff9bb06d9171c480e43
Rafael "Lestat" Kuhn <rafa.faelk@gmail.com>
2023-03-01 23:25:03 -0300

    fiz essa merda e é isso

---
+++ index.js
@@ -0,0 +1,120 @@
+// (a|b)+ cc(a|b|c)*
+
+const a = "a";
+const b = "b";
+const c = "c";
+
+/** @param {String} palavra @param {Number} indiceNaPalavra */
+const q0 = (palavra, indiceNaPalavra) => {
+	const terminouAPalavra = indiceNaPalavra == palavra.length;
+	if (terminouAPalavra) { terminouPalavraNoEstado("q0"); return; }
+
+	const charAtual = palavra[indiceNaPalavra];
+	console.log(`em: q0, estado: ${formataAPalavraBonitao(indiceNaPalavra)}`);
+
+	if (charAtual == a || charAtual == b)
+		q1(palavra, indiceNaPalavra + 1);
+	else
+		achouUmNaoDefinido();
+}
+
+/** @param {String} palavra @param {Number} indiceNaPalavra */
+const q1 = (palavra, indiceNaPalavra) => {
+	const terminouAPalavra = indiceNaPalavra == palavra.length;
+	if (terminouAPalavra) { terminouPalavraNoEstado("q1"); return; }
+	
+	const charAtual = palavra[indiceNaPalavra];
+	console.log(`em: q1, estado: ${formataAPalavraBonitao(indiceNaPalavra)}`);
+
+	if (charAtual == a || charAtual == b)
+		q1(palavra, indiceNaPalavra + 1);
+	else if (charAtual == c)
+		q2(palavra, indiceNaPalavra + 1)
+	else
+		achouUmNaoDefinido();
+}
+
+/** @param {String} palavra @param {Number} indiceNaPalavra */
+const q2 = (palavra, indiceNaPalavra) => {
+	const terminouAPalavra = indiceNaPalavra == palavra.length;
+	if (terminouAPalavra) { terminouPalavraNoEstado("q2"); return; }
+	
+	const charAtual = palavra[indiceNaPalavra];
+	console.log(`em: q2, estado: ${formataAPalavraBonitao(indiceNaPalavra)}`);
+
+	if (charAtual == c)
+		q3(palavra, indiceNaPalavra + 1);
+	else
+		achouUmNaoDefinido();
+}
+
+/** @param {String} palavra @param {Number} indiceNaPalavra */
+const q3 = (palavra, indiceNaPalavra) => {
+	const terminouAPalavra = indiceNaPalavra == palavra.length;
+	if (terminouAPalavra) { terminouPalavraNoEstado("q3"); return; }
+	
+	const charAtual = palavra[indiceNaPalavra];
+	console.log(`em: q3, estado: ${formataAPalavraBonitao(indiceNaPalavra)}`);
+
+	if (charAtual == a || charAtual == b || charAtual == c)
+		q3(palavra, indiceNaPalavra + 1);
+	else
+		achouUmNaoDefinido();
+}
+
+
+/** @param {String} estadoFinal */
+const terminouPalavraNoEstado = (estadoFinal) => {
+	alert(`terminou no estado ${estadoFinal}, aceito? ${estadoFinal == "q3"}, ver console!`);
+}
+
+const achouUmNaoDefinido = () => {
+	alert(`se fudeu mano indefinido essa porra ai, ver console!`);
+}
+
+
+
+/** @param {String} palavra */
+const criaFormatadorDePalavraBonitao = (palavra) => {
+	const arrayDaPalavraComEspacosAlternados = [];
+	arrayDaPalavraComEspacosAlternados.length = palavra.length * 2 + 1
+	
+	arrayDaPalavraComEspacosAlternados[0] = ' ';
+	for (let i = 0; i < palavra.length; ++i) {
+		const iStride = i * 2 + 1;
+		arrayDaPalavraComEspacosAlternados[iStride] = palavra[i];
+		arrayDaPalavraComEspacosAlternados[iStride+1] = ' ';
+	}
+
+	/** @param {Number} indiceBonito @returns {String} */
+	const funcaoInternaQueFormataBonitao = (indiceBonito) => {
+		indiceBonito = (indiceBonito * 2) + 1;
+
+		arrayDaPalavraComEspacosAlternados[0] = ' ';
+		for (let i = 0; i < palavra.length; ++i) {
+			const iStride = i * 2 + 1;
+			arrayDaPalavraComEspacosAlternados[iStride+1] = ' ';
+		}
+
+		arrayDaPalavraComEspacosAlternados[indiceBonito - 1] = '[';
+		arrayDaPalavraComEspacosAlternados[indiceBonito + 1] = ']';
+		
+		return arrayDaPalavraComEspacosAlternados.join("");
+	}
+
+	return funcaoInternaQueFormataBonitao;
+}
+
+const palavra
+// = "";
+// = "a";
+// = "c";
+// = "caaa";
+// = "aaaaaaaaaabbbbbbbbbbbbc";
+// = "abc";
+// = "abcabcabcabc";
+= "abccababababa";
+// = "ababababac";
+
+const formataAPalavraBonitao = criaFormatadorDePalavraBonitao(palavra);
+q0(palavra, 0);
